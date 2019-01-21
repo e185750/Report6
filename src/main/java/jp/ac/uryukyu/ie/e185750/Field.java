5cfd60d9f31004baa5fa3c8cfd28f33122aeeedf
Syouya Takahashi <e185750@ie.u-ryukyu.ac.jp>
2019-01-22 03:13:23 +0900

    1st commit

--- src/main/java/jp/ac/uryukyu/ie/e185750/Field.java
+++ src/main/java/jp/ac/uryukyu/ie/e185750/Field.java
@@ -39,16 +39,16 @@ public class Field {
     }
 
     public void feature() {
-        String[][] board = new String[ynum][xnum];
-        for (Koma koma : this.komalist) {
-            int[] pos = koma.getPosition();
-            String state = koma.getState();
-            board[pos[1]][pos[0]] = state;
-        }
-        System.out.println("\n\t0\t1\t2\t3\t4\t5\t6\t7\n");
-        for (int y = 0; y < board.length; y++) {
-            System.out.print(y + "\t");
-            for (int x = 0; x < board[0].length; x++) {
+                String[][] board = new String[ynum][xnum];
+                for (Koma koma : this.komalist) {
+                    int[] pos = koma.getPosition();
+                    String state = koma.getState();
+                    board[pos[1]][pos[0]] = state;
+                }
+                System.out.println("\n\t0\t1\t2\t3\t4\t5\t6\t7\n");
+                for (int y = 0; y < board.length; y++) {
+                    System.out.print(y + "\t");
+                    for (int x=0; x < board[0].length; x++) {
                 String b = board[y][x];
                 System.out.print(b + "\t");
             }
